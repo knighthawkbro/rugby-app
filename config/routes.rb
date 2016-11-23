cf478cb45e4e392ea2d3deceac993f6ddd5b828e
Brady Walsh <bradywalsh@comcast.net>
2016-11-23 15:30:04 +0000

    added users resources to route file and some testing params for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   # resources :static_pages
-  
-  get 'users/new'
+  resources :users
 
   # Sets the default homepage when users access https://rugby-project-bradywalsh.c9users.io
   root 'static_pages#home'
+  get '/users/new', to: 'users#new'
   
   # Changed the default URL/PATH of the Static pages
-  get '/about',     to: 'static_pages#about'
-  get '/contact',   to: 'static_pages#contact'
-  get '/schedule',  to: 'static_pages#schedule'
-  get '/roster',    to: 'static_pages#roster'
-  get '/media',     to: 'static_pages#media'
-  get '/awards',    to: 'static_pages#awards'
+  get    '/about',     to: 'static_pages#about'
+  get    '/contact',   to: 'static_pages#contact'
+  get    '/schedule',  to: 'static_pages#schedule'
+  get    '/roster',    to: 'static_pages#roster'
+  get    '/media',     to: 'static_pages#media'
+  get    '/awards',    to: 'static_pages#awards'
+  get    '/login',     to: 'sessions#new'
+  post   '/login',     to: 'sessions#create'
+  delete '/logout',    to: 'sessions#destroy'
 end
